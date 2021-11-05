5a22cf2c6f8c46a24a4f24c7d720cc0aa44c7db9
DexCruise <dexter.e.cruise@gmail.com>
2021-11-05 15:43:21 -0700

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,8 +7,16 @@ public class Main {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel records = new JPanel();
-        JTable recordTable = new JTable();
 
+        JTable recordTable = new JTable();
+        recordTable.addColumn(new TableColumn(0));
+        recordTable.addColumn(new TableColumn(1));
+        recordTable.setRowHeight(10);
+        recordTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+        records.add(recordTable);
+
+        frame.add(recordTable);
 
         // System.out.printf("Tim's homework from yesterday: %f%n", Tim.getScore("yesterday's homework"));
         frame.pack();
