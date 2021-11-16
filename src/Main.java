e28978c01d29c79e72c43f5d8707be17522b5aed
dexter <dexter.e.cruise@gmail.com>
2021-11-16 12:39:42 -0800

    yay it works a little now

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@ import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
+
+    private static final String ADD_STUDENT = "Add Student";
+    private static final String DEL_STUDENT = "Remove Student";
+    private static final String ADD_ASSIGNMENT = "Add Assignment";
+    private static final String DEL_ASSIGNMENT = "Remove Assignment";
+
+    private static JTable studentTable;
+    private static TableModel dataModel;
+    private static StudentList students;
 
     private static final class NameDisplayStyle {
         public static final int FIRST_FIRST = 0, LAST_FIRST = 1;
@@ -16,7 +27,7 @@ public class Main {
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
         int nameDisplayStyle = NameDisplayStyle.LAST_FIRST;
-        TableModel dataModel = new AbstractTableModel() {
+        dataModel = new AbstractTableModel() {
             public int getColumnCount() { return students.getAssignmentCount() + 2; }
             public int getRowCount() { return students.getStudentCount(); }
             public boolean isCellEditable(int row, int col) { return true; }
@@ -76,11 +87,6 @@ public class Main {
         JFrame frame = new JFrame("StudentRecords v0.0.0 by Dexter");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
-
-
-
-
         JScrollPane scrollPane = new JScrollPane(table,
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -90,20 +96,49 @@ public class Main {
         scrollPane.getVerticalScrollBar().setUnitIncrement(10);
         scrollPane.getHorizontalScrollBar().setUnitIncrement(10);
 
-        frame.add(scrollPane);
+        ButtonListener buttonListener = new ButtonListener();
 
+        JPanel buttonPanel = new JPanel();
+
+        JButton addAssignmentButton = new JButton("Add Assignment");
+        JButton addStudentButton = new JButton("Add Student");
+        JButton deleteAssignmentButton = new JButton("Delete Assignment");
+        JButton deleteStudentButton = new JButton("Delete Student");
+
+        addAssignmentButton.setActionCommand(ADD_ASSIGNMENT);
+        addStudentButton.setActionCommand(ADD_STUDENT);
+        deleteAssignmentButton.setActionCommand(DEL_ASSIGNMENT);
+        deleteStudentButton.setActionCommand(DEL_STUDENT);
+
+        addAssignmentButton.addActionListener(buttonListener);
+        addStudentButton.addActionListener(buttonListener);
+        deleteAssignmentButton.addActionListener(buttonListener);
+        deleteStudentButton.addActionListener(buttonListener);
+
+        buttonPanel.add(addAssignmentButton);
+        buttonPanel.add(deleteAssignmentButton);
+        buttonPanel.add(addStudentButton);
+        buttonPanel.add(deleteStudentButton);
+
+        buttonPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
+        buttonPanel.setMinimumSize(new Dimension(200, 20));
+
+        frame.add(buttonPanel, BorderLayout.BEFORE_FIRST_LINE);
+        frame.add(scrollPane, BorderLayout.CENTER);
         frame.pack();
 
         frame.setVisible(true);
     }
 
     /**
+     * @param StudentCount number of students to generate
+     * @param AssignmentCount number of assignments to generate
      *
-     * @return returns a randomly generated StudentList with a few assignments and
+     * @return returns a randomly generated StudentList
      */
 
     public static StudentList makeStudentListForTesting(int StudentCount, int AssignmentCount) {
-        StudentList students = new StudentList();
+        students = new StudentList();
 
         ArrayList<String> names = new ArrayList<>();
 
@@ -144,12 +179,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        StudentList students = makeStudentListForTesting(30, 100);
+        StudentList students = makeStudentListForTesting(30, 5);
 
-        JTable studentTable = createStudentTable(students);
+        studentTable = createStudentTable(students);
 
         System.out.println(studentTable.toString().replaceAll(",", ",\n"));
         displayGUI(studentTable);
 
+    }
+
+    public static class ButtonListener implements ActionListener {
+        public void actionPerformed(final ActionEvent ev) {
+            switch (ev.getActionCommand()) {
+                case ADD_ASSIGNMENT -> students.addAssignment("New Assignment");
+
+                case ADD_STUDENT -> students.addStudent(-1, new Name("Student", "Name"));
+
+                case DEL_ASSIGNMENT, DEL_STUDENT -> System.out.println("No Deleting Yet!");
+            }
+
+            studentTable.setModel(dataModel);
+        }
     }
 }
